eea0caa1986f46871d4ae5fb52a76d18278caf0f
Muhammad Khan <muhammad.khan-52@student.manchester.ac.uk>
2025-05-13 10:24:41 +0100

    Run Length Encoding

---
+++ beginner/run_length_encoding.hs
@@ -0,0 +1,16 @@
+-- Given a list returns its run length encoding 
+
+encode [] last last_freq = []
+
+encode (x:xs) last last_freq 
+    | last_freq == 0 = encode xs x 1  
+    | x == last = encode xs last (last_freq+1)
+    | otherwise = (last, last_freq) : encode xs x 1  
+
+
+a = "aaaabccaadeee"
+b = "abcdef"
+
+c = "aaabccddefg"
+
+main  = print( encode a 'a' 0)
